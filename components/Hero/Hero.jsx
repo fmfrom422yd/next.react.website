79ceed8c58d3d33d056feeba30c59bcca725724c
fmfrom422yd <KTC2243050006@edu.kyoto-tech.ac.jp>
2023-01-27 16:01:09 +0900

    fix: _hero change filename

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import styles from './Hero.module.css'
+export default function Hero( {title, subtitle, imageOn = false }){
+    return (
+        <div>
+            <div className={styles.text}>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={styles.subtitle}>{subtitle}</p>
+            </div>
+            
+        {imageOn && <figure> [画像] </figure>}
+        </div>
+    )
+}
